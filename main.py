49808eb89b8b7f3bd9c18683f8b699cfa5f1fbf2
MihirMagikarp <mihir.majgaonkar@gmail.com>
2020-08-28 22:04:57 +0000

    started making the weapon functions, still more to ba added

---
+++ main.py
@@ -0,0 +1,15 @@
+inventory = []
+item_get = ''
+
+def sword():
+  inventory.append('sword')
+  print('\nyou got a sword')
+def axe():
+  inventory.append('axe')
+  print('\nyou got an axe.')
+def potato():
+  inventory.append('potato')
+  print('\nyou got a potato')
+
+
+def inventory_sys():
